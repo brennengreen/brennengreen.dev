3ef1816f6bb3d005f530401cf2cee67352908aa0
Brennen Green <brennengreen@outlook.com>
2019-03-06 17:19:46 -0500

    Deploying to Heroku

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const public = path.join(__dirname, 'dist');
+const port = process.env.PORT || 5000;
 
 app.use(express.static(public));
-
 app.get('/', function(req, res) {
     res.sendFile(path.join(public ,'index.html'));
 })
 
-app.listen(1234);
-console.log("Listening on port 1234");
+app.listen(port, (req, res) => {
+    console.log(`Listening on port ${port}`);
+});
