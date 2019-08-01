b37f010ea89017af2a50ba5336d67696e51d9154
Brennen Green <brennengreen@outlook.com>
2019-08-01 09:55:19 -0400

    Validation progress for admin panel

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var bodyParser = require('body-parser');
 const public = path.join(__dirname, 'dist');
 const port = process.env.PORT || 5000;
 
@@ -9,19 +10,35 @@ var processReq = function (req, res, next) {
     console.log(res);
     next();
 }
+function validToken(token) {
+    if (token === "iambrennen!") {
+        return true
+    }
+    return false
+}
 
-app.use(processReq);
+//app.use(processReq);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use(express.static(public));
+
+app.post('/admin/panel.html', function(req, res) {
+    console.log("post received: %s", req.body.AdminToken);
+    if (validToken(req.body.AdminToken)) {
+        res.sendFile(path.join(public, 'admin', 'panel.html'));
+    } else {
+        res.sendFile(path.join(public ,'admin','index.html'))
+    }
+})
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(public ,'index.html'));
 })
 
 app.get('admin', function(req, res) {
     res.sendFile(path.join(public ,'admin','index.html'));
-})
-
-app.post('admin/panel.html', function(req, res) {
-    res.sendFile(path.join(public, 'admin', 'panel.html'));
 })
 
 app.listen(port, (req, res) => {
