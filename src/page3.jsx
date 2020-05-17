debd20d2a6f1c2904a70c4475ec90dcc48abe569
Brennen Green <brennengreen@outlook.com>
2020-05-17 07:36:15 -0400

    t

--- src/page3.jsx
+++ src/page3.jsx
@@ -4,11 +4,13 @@ import "../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min
 
 
 const products = [
+    {'name':"Toon Shader", 'desc':"OpenGL Toon Shader Implementation", 'date':"04/14/2020",'tool':"OpenGL, C++",link:"https://github.com/brennengreen/Shaders" },
     {'name':"2D Procedural Generation", 'desc':"Conway's game of life based proc gen", 'date':"01/10/2020",'tool':"Unity, C#",link:"https://github.com/brennengreen/2D-Procedural-Generation" },
     {'name':"Ditto", 'desc':"A media assisting bot for Discord", 'date':"06/28/2019",'tool':"Python",link:"https://github.com/brennengreen/ditto" },
     {'name':"GoList", 'desc':"WebScraper that stores data in postgresql db", 'date':"03/21/2019",'tool':"Golang, SQL",link:"https://github.com/brennengreen/golist" },
     {'name':"My Portfolio", 'desc':"A website to show off my personality and technical interests", 'tool':"WebDev w/ JS",'link':"https://github.com/brennengreen/brennengreen.dev"},
     {'name':"pystudy", 'desc':"Virtual flash cards", 'date':"08/28/2018",'tool':"Python",link:"https://github.com/brennengreen/pystudy" },
+    {'name':"EpicPwnBot", 'desc':"Joke tracking discord bot", 'date':"05/01/2020",'tool':"JavaScript, AWS",link:"https://github.com/brennengreen/EpicPwnBot" },
     {'name':"pybrarian", 'desc':"Wiki-web-scraper, provides info such as Author, Length, general summary of any book user provides on CLI",
             'date':"08/05/2018",'tool':"Python", link:"https://github.com/brennengreen/pybrarian"},
     {'name':"pytax", 'desc':"A tax calculator which uses the TaxJar sales api to help users figure out tax costs",
